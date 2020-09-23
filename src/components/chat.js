51c3c3ce8a53c7813673a77d39cec1cee1217f52
SDen4 <asw325@yandex.ru>
2020-09-23 21:43:13 +0500

    new message field

--- src/components/chat.js
+++ src/components/chat.js
@@ -9,7 +9,7 @@ class Chat extends Component {
         return (
             <div className='chat'>
                 <h2 className='chat__subtitle'>{title}</h2>
-                <ul className='chat__wrapper'>
+                <ul className='chat__messages_list'>
                    {messagesList}
                 </ul>
                 <div className='chat__new_message'>
@@ -17,6 +17,11 @@ class Chat extends Component {
                         className='chat__input'
                         placeholder='Введите сообщение'
                     ></input>
+                    <div className="chat__button_wrapper">
+                        <button className="button__send_message">
+                            <div className="button__send_message_triangle"></div>
+                        </button>
+                    </div>
                 </div>
             </div>
         )
